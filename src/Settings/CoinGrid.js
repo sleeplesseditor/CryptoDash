35c903173699b698fff5e81e512406a736cefbda
sleeplesseditor <josh@bryson.me.uk>
2018-11-11 16:56:13 +0000

    Responsive grid setup

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,13 +5,13 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
     grid-gap: 15px;
     margin-top: 40px;
 `
 
 function getCoinsToDisplay(coinList, topSection, favorites){
-    return topSection ? favorites : Object.keys(coinList).slice(0, topSection ? 10 : 100);
+    return topSection ? favorites : Object.keys(coinList).slice(0, topSection ? 10 : 200);
 }
 
 export default function({topSection}) {
