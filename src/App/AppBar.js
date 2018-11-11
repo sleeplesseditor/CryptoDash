0c189122c7072b274449e894f2b29686f6c1991b
sleeplesseditor <josh@bryson.me.uk>
2018-11-11 02:07:48 +0000

    React Context implemented

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import {AppContext} from './AppProvider';
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
 
 const Bar = styled.div`
     display: grid;
+    margin-bottom: 40px;
     grid-template-columns: 180px auto 100px 100px;
 `
 
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        text-shadow: 0px 0px 60px #03ff03;
+    `}
+`
+
+function toProperCase(lower) {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({name}) {
+    return (
+        <AppContext.Consumer>
+            {({page, setPage}) => (
+                <ControlButtonElem 
+                    active={page === name}
+                    onClick={() => setPage(name)}
+                >
+                    {toProperCase(name)}
+                </ControlButtonElem>
+            )}
+        </AppContext.Consumer>
+    )
+}
+
 export default function() {
-    return <Bar>
-                <div>CryptoDash </div>
-                <div/>
-                <div>Dashboard</div>
-                <div>Settings</div>
-            </Bar>
+    return (
+        <Bar>
+            <Logo>CryptoDash </Logo>
+            <div/>
+            <ControlButton active name="Dashboard"/>
+            <ControlButton name="Settings"/>
+        </Bar>
+    )
 }
